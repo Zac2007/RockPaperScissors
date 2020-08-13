3383234e324eead30773cb643a0cd99afa154aa2
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-13 21:16:58 +0530

    Update Rockpaperscissors.py

--- Rockpaperscissors.py
+++ Rockpaperscissors.py
@@ -1,1 +1,7 @@
-
+import rps
+from random import choice
+a = ["paper", "rock", "scissors"]
+# User enters the choice
+# Computer picks from those choices, using choice(a)
+# Run rps function, determine who wins. Pass in rps parameters as shown earlier.
+#Add play again function(if wanted)
